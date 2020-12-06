d30eafc7ce5f5305cc696c62032dd31bc3462969
obedkristiaji <57378618+obedkristiaji@users.noreply.github.com>
2020-12-06 19:33:24 +0700

    FINAL VERSION (#6)
    * add sensor
    
    * fix missing tiles
    
    * fix interfaces and menus
    
    * night mode theme
    
    * FINAL VERSION
    
    Co-authored-by: obedkristiaji <myemail>

--- app/src/main/java/com/example/pianotiles/view/SettingFragment.kt
+++ app/src/main/java/com/example/pianotiles/view/SettingFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.pianotiles.R
 import com.example.pianotiles.databinding.FragmentSettingBinding
@@ -32,18 +33,23 @@ class SettingFragment : DialogFragment() {
 
         this.binding.btnRed.setOnClickListener {
             this.presenter.setColor(R.color.red)
+            Toast.makeText(context, "Background : Red", Toast.LENGTH_SHORT).show()
         }
         this.binding.btnGreen.setOnClickListener {
             this.presenter.setColor(R.color.green)
+            Toast.makeText(context, "Background : Green", Toast.LENGTH_SHORT).show()
         }
         this.binding.btnBlue.setOnClickListener {
             this.presenter.setColor(R.color.blue)
+            Toast.makeText(context, "Background : Blue", Toast.LENGTH_SHORT).show()
         }
         this.binding.btnNormal.setOnClickListener {
             this.presenter.setLevel(1)
+            Toast.makeText(context, "Difficulty : Normal", Toast.LENGTH_SHORT).show()
         }
         this.binding.btnHard.setOnClickListener {
             this.presenter.setLevel(2)
+            Toast.makeText(context, "Difficulty : Hard", Toast.LENGTH_SHORT).show()
         }
         this.binding.btnBack.setOnClickListener {
             dismiss()
